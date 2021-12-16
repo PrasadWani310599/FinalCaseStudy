bbe268e0d9b08e9166f97ee4743e3b570fbf5afc
Prasad wani <waniprasadk@gmail.com>
2021-12-16 11:44:35 +0530

    Initial commit

---
+++ src/test/java/com/example/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.PaymentGateway;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
